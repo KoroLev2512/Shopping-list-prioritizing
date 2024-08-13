cbbbe4ab9068cb5d55ab40208f2d86bb4d10082f
korolev_2512 <korolev-2512@mail.ru>
2024-08-13 16:24:14 +0300

    Добавил таблицы

---
+++ add_product/models/goods.py
@@ -0,0 +1,8 @@
+from django.db import models
+from .users import UsersModel
+
+
+class GoodsModel(models.Model):
+    name = models.CharField(verbose_name='Товар', max_length=255)
+    user = models.ForeignKey(UsersModel, on_delete=models.CASCADE, verbose_name='Пользователь')
+    priority = models.IntegerField(verbose_name='Приоритет')
